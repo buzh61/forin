cf6c77f1524f8d7294e864f4a14608d1e4dfd8ac
buzh.61 <buzh.61@gmail.com>
2022-06-20 20:00:07 +0300

    eslint fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,32 @@
-const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40};
+const obj = {
+  name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+};
 
 export default function orderByProps(object, sorting) {
-    const objArray = [];
+  const objArray = [];
 
-    for (const key in object) {
-        if (Object.prototype.hasOwnProperty.call(obj, key)) {
-            const value = obj[key];
-            objArray.push({key, value});
-        }
+  for (const key in object) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const value = obj[key];
+      objArray.push({ key, value });
     }
+  }
 
-    return objArray.sort((a, b) => {
-        if (sorting.includes(a.key) && !sorting.includes(b.key)) {
-            return -1;
-        }
-        if (sorting.includes(b.key) && !sorting.includes(a.key)) {
-            return 1;
-        }
-        if (sorting.includes(a.key) && sorting.includes(b.key)) {
-            return sorting.indexOf(a.key) - sorting.indexOf(b.key);
-        }
-        if (a.key < b.key) {
-            return -1;
-        }
-        return 1;
-    });
+  return objArray.sort((a, b) => {
+    if (sorting.includes(a.key) && !sorting.includes(b.key)) {
+      return -1;
+    }
+    if (sorting.includes(b.key) && !sorting.includes(a.key)) {
+      return 1;
+    }
+    if (sorting.includes(a.key) && sorting.includes(b.key)) {
+      return sorting.indexOf(a.key) - sorting.indexOf(b.key);
+    }
+    if (a.key < b.key) {
+      return -1;
+    }
+    return 1;
+  });
 }
 
-orderByProps(obj, ["name", "level"])
+orderByProps(obj, ['name', 'level']);
--- test/test.js
+++ test/test.js
@@ -1,21 +1,21 @@
-import orderByProps from "../src/js/app";
+import orderByProps from '../src/js/app';
 
 test('test sorting', () => {
-    const person = {
-        name: 'мечник',
-        health: 10,
-        level: 2,
-        attack: 80,
-        defence: 40,
-    };
+  const person = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40,
+  };
 
-    const expectedData = [
-        { key: 'name', value: 'мечник' },
-        { key: 'level', value: 2 },
-        { key: 'attack', value: 80 },
-        { key: 'defence', value: 40 },
-        { key: 'health', value: 10 },
-    ]
-    const sorting = orderByProps(person, ['name', 'level']);
-    expect(sorting).toEqual(expectedData);
+  const expectedData = [
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+  ];
+  const sorting = orderByProps(person, ['name', 'level']);
+  expect(sorting).toEqual(expectedData);
 });
