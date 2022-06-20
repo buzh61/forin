b75c5838a0fc52ee51f071451496ec629c54be01
buzh.61 <buzh.61@gmail.com>
2022-06-20 19:57:30 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,30 @@
+const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40};
+
+export default function orderByProps(object, sorting) {
+    const objArray = [];
+
+    for (const key in object) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            const value = obj[key];
+            objArray.push({key, value});
+        }
+    }
+
+    return objArray.sort((a, b) => {
+        if (sorting.includes(a.key) && !sorting.includes(b.key)) {
+            return -1;
+        }
+        if (sorting.includes(b.key) && !sorting.includes(a.key)) {
+            return 1;
+        }
+        if (sorting.includes(a.key) && sorting.includes(b.key)) {
+            return sorting.indexOf(a.key) - sorting.indexOf(b.key);
+        }
+        if (a.key < b.key) {
+            return -1;
+        }
+        return 1;
+    });
+}
+
+orderByProps(obj, ["name", "level"])
